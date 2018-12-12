fd511e77ac40bc885674602ac5b843fd05833e9e
heshangbuxitou <1581277795@qq.com>
2018-12-12 17:19:25 +0800

    IU-2018.2 <Administrator@USER-20171113OZ Create markdown.xml, ide.general.xml, git.xml, _@user_Default.icls, project.default.xml, rainbow_brackets.xml, Default copy.xml, other.xml, Default.xml, applicationLibraries.xml, Monokai Sublime Text 3.icls, find.xml, ui.lnf.xml, gradle.settings.xml, filetypes.xml, keymap.xml, Solarized Light (Darcula).icls, plugin_ui.xml, colors.scheme.xml, _@user_Material Oceanic.icls, _@user_Darcula.icls, junitgenerator-app-settings.xml, databaseDrivers.xml, mavenVersion.xml, Ladies Night 2.icls, javarebel.xml, Default.xml, laf.xml, Atom One Dark.icls, _@user_Material Darker.icls, Class.java, jdk.table.xml, debugger.renderers.xml, path.macros.xml, Solarized Dark.icls, Solarized Dark (Darcula).icls, Xcode Midnight.icls, editor.codeinsight.xml, Solarized Light.icls, databaseSettings.xml, toStringTemplates.xml, editor.xml, debugger.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,12 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+ * @ClassName ${NAME}
+ * @Description TODO
+ * @Author zhu.yong
+ * @Date  ${DATE} ${TIME}
+ * @Version  1.0
+ */
+public class ${NAME} {
+}
+
